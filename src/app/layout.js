2ccbac06b39bb1abef7a0278b2b2d154352970d8
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-09-17 13:51:52 +0600

    meta tag updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,31 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta name="robots" content={metadata.robots} />
+        <meta name="robots" content={metadata.robots} />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Person",
+              "name": "Shakiqur Rahman",
+              "url": "https://shakiqur-rahman.vercel.app",
+              "jobTitle": "Web Developer",
+              "worksFor": {
+                "@type": "Organization",
+                "name": "DevIt BD"
+              }
+            }),
+          }}
+        />
+      </Head>
+      </Head>
       <body>
         <main className={inter.className}>
           <Header />
